9b6893a174bf32ae25c781d981b9a5563b054746
Harish Manana <harish.manana@stackroute.in>
2021-03-03 16:08:01 +0530

    First Commit

---
+++ SpringBootRestCourseCrud_BackEnd/src/main/java/com/stackroute/SpringBootCrudApp.java
@@ -0,0 +1,13 @@
+package com.stackroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudApp.class, args);
+	}
+
+}
